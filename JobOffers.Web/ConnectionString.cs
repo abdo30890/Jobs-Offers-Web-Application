5c83649bb92d3a5d8cce1a116b759071a9196157
abdo30890 <60926916+abdo30890@users.noreply.github.com>
2020-02-17 02:02:27 +0200

    createConnectionString

---
+++ JobOffers.Web/ConnectionString.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace JobOffers.Web
+{
+    public  static class ConnectionString
+    {
+        public static string Connection()
+        {
+            var builder = new SqlConnectionStringBuilder()
+
+            {
+                DataSource = @"Abdelrahman\SQLEXPRESS",
+                InitialCatalog = "JobOffersDB",
+                UserID = "sa",
+                Password = "WikiUX@01000"
+
+            };
+
+            return builder.ConnectionString;
+
+        }
+    }
+}
